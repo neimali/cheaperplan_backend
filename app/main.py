c38809d37802e477bcfbad2c6c5f1eb83b157227
neimali <gaoyinghan1997@gmail.com>
2025-06-17 02:19:29 +0000

    helloworld

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(root_path="/cheap_api")
 
+origins = [
+    "http://localhost",
+    "http://localhost:8081", 
+    "http://127.0.0.1",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 白名单
+    allow_credentials=True,
+    allow_methods=["*"],    # 允许所有方法
+    allow_headers=["*"],    # 允许所有头部
+)
+
 @app.get("/hello")
 def hello():
-    return {"msg": "Hello FastAPI with Poetry"}
+    return {"msg": "Hello from FastAPI"}
